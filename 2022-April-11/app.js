fa017664ae8e6fcfcdaae73ab2697ce2af2d7242
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-04-12 12:06:11 -0500

    CC from 4-11-22

---
+++ 2022-April-11/app.js
@@ -0,0 +1,15 @@
+//Training JS #1: create your first JS function and print "Hello World!"
+
+// Example
+// function printWordToScreen() {
+//     var somewords = "This is an example."
+//     console.log(somewords)
+// }
+
+
+
+//refer to the example and write your first JS function
+function helloWorld() {
+    var str = 'Hello World!'
+    console.log(str)
+}
