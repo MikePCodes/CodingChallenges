c3db4fb5852ddd516983b6a68bfabb2ce790197c
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-04-19 19:09:23 -0500

    CC from 4-19-22

---
+++ 2022-April-19/app2.js
@@ -0,0 +1,12 @@
+// Twice as old
+
+// Your function takes two arguments:
+
+// 1. current father's age (years)
+// 2. current age of his son (years)
+
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs((sonYearsOld * 2) - dadYearsOld)
+}
