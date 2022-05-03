a07b744cfde71bffe14ad9fa62d9f93c799bec86
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-05-02 21:39:46 -0500

    CC from 5-2-22

--- 2022-May-2/app.js
+++ 2022-May-2/app.js
@@ -17,4 +17,21 @@
 // dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
 // dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
 
-
+function DNAStrand(dna) {
+    const complementaryDna = dna
+        .split('')
+        .map(string => {
+            switch (string) {
+                case 'A':
+                    return 'T'
+                case 'T':
+                    return 'A'
+                case 'G':
+                    return 'C'
+                case 'C':
+                    return 'G'
+            }
+        })
+        .join('')
+    return complementaryDna
+}
