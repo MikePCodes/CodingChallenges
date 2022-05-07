70ff3f2fa30fc7d54abf6b18632a5e1189f35751
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-05-07 16:53:02 -0500

    CC from 5-7-22

--- 2022-May-7/app.js
+++ 2022-May-7/app.js
@@ -9,3 +9,11 @@
 
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    const a = s1.split('');
+    const b = s2.split('');
+
+    return [...new Set([...a, ...b])].sort().join('');
+
+}
