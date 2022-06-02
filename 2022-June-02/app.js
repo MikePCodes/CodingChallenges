e247afb926fea1648d1f908c7c1fbb38487aaf4f
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-06-02 18:11:54 -0500

    CC from 6-2-22

--- 2022-June-02/app.js
+++ 2022-June-02/app.js
@@ -10,3 +10,12 @@
 //I is misinterpreted as 1
 //The test cases contain numbers only by mistake.
 
+function correct(string) {
+    return string.split('').map(char => {
+        if (char === '5') return 'S'
+        if (char === '0') return 'O'
+        if (char === '1') return 'I'
+        return char
+    })
+        .join('')
+}
