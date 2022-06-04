8fec8910348d8c14f391e2edc49478e8b678fe32
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-06-04 08:36:13 -0500

    CC from 6-4-22

--- 2022-June-04/app.js
+++ 2022-June-04/app.js
@@ -23,3 +23,11 @@
 // 55, 0 --> 0
 // 20, 2 --> 0
 // *Use Comparison and Logical Operators.
+
+const finalGrade = (exam, projects) => {
+    if (exam > 90 || projects > 10) return 100
+    if (exam > 75 && projects >= 5) return 90
+    if (exam > 50 && projects >= 2) return 75
+
+    return 0
+}
