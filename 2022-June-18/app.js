b51b417187ccd5cac9d4025bbde62b4a3a478a44
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-06-18 19:19:07 -0500

    CC from 6-18-22

--- 2022-June-18/app.js
+++ 2022-June-18/app.js
@@ -6,3 +6,8 @@
 
 // If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+function hoopCount(n) {
+    if (n >= 10) return 'Great, now move on to tricks'
+    return 'Keep at it until you get it'
+}
