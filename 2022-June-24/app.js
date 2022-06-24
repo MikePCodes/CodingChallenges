22ff4598a52498e85328949c869157c366e66051
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-06-24 08:26:13 -0500

    CC from 6-24-22

--- 2022-June-24/app.js
+++ 2022-June-24/app.js
@@ -10,3 +10,6 @@
 
 // ["Eyes", "Glasses", "Monocles", "Telescopes"]
 
+//All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+const sortByLength = array => [...array].sort((a, b) => a.length - b.length)
