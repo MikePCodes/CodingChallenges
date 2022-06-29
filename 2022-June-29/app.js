57ce83022753898a020e8f14cd00b55a83e07e6f
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-06-29 07:43:12 -0500

    CC from 6-29-22

--- 2022-June-29/app.js
+++ 2022-June-29/app.js
@@ -15,3 +15,7 @@
 // Notes:
 // The first argument can be an empty string
 // The second string argument will always be of length 1
+
+function strCount(str, letter) {
+    return str.split(letter).length - 1
+}
