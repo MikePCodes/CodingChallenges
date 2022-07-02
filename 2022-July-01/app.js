455e086a0e17f213a78b4e69786e8d7b28d59a4c
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-07-01 22:41:40 -0500

    CC from 7-1-22

--- 2022-July-01/app.js
+++ 2022-July-01/app.js
@@ -23,3 +23,18 @@
 // digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
 // digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 // digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+function digPow(n, p) {
+    const summation = String(n)
+        .split('')
+        .map(char => Number(char))
+        .reduce((a, b) => {
+            const result = a + b ** p
+            p++
+            return result
+        }, 0)
+
+    const k = summation / n
+
+    return Number.isInteger(k) ? k : -1
+}
