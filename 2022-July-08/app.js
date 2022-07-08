8f5deda2444f5f4e3382528d1f552a95084ac4b2
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-07-08 05:01:47 -0700

    CC from 7-8-22

--- 2022-July-08/app.js
+++ 2022-July-08/app.js
@@ -9,3 +9,8 @@
 // findSum(5) should return 8 (3 + 5)
 
 // findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+const findSum = n =>
+    Array.from({ length: n }, (_, index) => index + 1)
+        .filter(number => number % 3 === 0 || number % 5 === 0)
+        .reduce((total, value) => total + value, 0)
