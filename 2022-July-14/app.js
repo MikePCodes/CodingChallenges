4b3173a43cba58b16b453f9a3c85e10a853d9b76
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-07-14 06:45:55 -0700

    CC from 7-14-22

--- 2022-July-14/app.js
+++ 2022-July-14/app.js
@@ -12,3 +12,10 @@
 
 // Example
 // Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+function pipeFix(numbers) {
+    return Array.from(
+        { length: numbers[numbers.length - 1] - numbers[0] + 1 },
+        (_, i) => numbers[0] + i
+    )
+}
