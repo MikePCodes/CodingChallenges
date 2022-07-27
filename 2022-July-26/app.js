0bc015779264077f0f037deb875ef9c6bef03724
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-07-26 19:51:32 -0700

    CC from 7-26-22

--- 2022-July-26/app.js
+++ 2022-July-26/app.js
@@ -3,3 +3,13 @@
 // Write a function which calculates the average of the numbers in a given list.
 
 // Note: Empty arrays should return 0.
+
+function find_average(array) {
+    if (array.length === 0) {
+        return 0
+    }
+    const result = array.reduce((a, b) => a + b, 0)
+    const totalItems = array.length
+
+    return result / totalItems
+}
