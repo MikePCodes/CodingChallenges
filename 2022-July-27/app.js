f31dbaa3d3a8ca2274e3f09e6fd25e3b60fe1dea
Michael Poellnitz <Michael_Poellnitz@yahoo.com>
2022-07-27 20:03:36 -0700

    CC from 7-27-22

--- 2022-July-27/app.js
+++ 2022-July-27/app.js
@@ -5,3 +5,11 @@
 
 // Example
 // Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+var capitals = function (word) {
+    var caps = [];
+    for (var i = 0; i < word.length; i++) {
+        if (word[i].toUpperCase() == word[i]) caps.push(i);
+    }
+    return caps;
+};
